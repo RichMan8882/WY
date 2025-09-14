392a7b85dba61db510ad372adf3921b720daf91c
baimao <baimao@gmail.com>
2025-09-14 14:28:18 +0800

    update  live-video

--- components/liveVideo.vue
+++ components/liveVideo.vue
@@ -20,9 +20,9 @@
 
       <!-- 自定义声音控制按钮 -->
       <div class="custom-sound-control">
-        <button @click="toggleMute" class="sound-button" :class="{ 'muted': props.isMuted }"
-          :title="props.isMuted ? '打开声音' : '关闭声音'">
-          <svg v-if="props.isMuted" class="sound-icon" viewBox="0 0 24 24" fill="currentColor">
+        <button @click="toggleMute" class="sound-button" :class="{ 'muted': isMuted }"
+          :title="isMuted ? '打开声音' : '关闭声音'">
+          <svg v-if="isMuted" class="sound-icon" viewBox="0 0 24 24" fill="currentColor">
             <path
               d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z" />
           </svg>
@@ -41,7 +41,7 @@
         </div>
         <div class="chat-btm">
           <div class="chat-lis">
-            <div class="chat-item" v-for="item in 9">
+            <div class="chat-item" v-for="item in 20">
               <div class="chat-title">
                 <div class="title-user">
                   {{ '用戶656516' }}
@@ -108,10 +108,6 @@ export default {
       default: 5
     },
     portraitMode: {
-      type: Boolean,
-      default: true
-    },
-    isMuted: {
       type: Boolean,
       default: true
     }
