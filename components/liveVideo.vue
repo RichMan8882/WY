12a65a69224f3e8c77de05cf8f5133dc80409f03
baimao <baimao@gmail.com>
2025-09-14 19:09:07 +0800

    live-edit

--- components/liveVideo.vue
+++ components/liveVideo.vue
@@ -185,7 +185,7 @@ const initializePlayer = async () => {
 
   try {
     // 初始化播放器（Vue3 需确保 DOM 已挂载）
-    player.value = videojs(videoPlayer.value, obj, () => {
+    player.value = videojs(videoPlayer.value, obj.merge, () => {
       console.log('[VideoPlayer] 播放器初始化完成');
       player.value.ready(() => {
         setupPlayerEvents(); // 绑定事件
