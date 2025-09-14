b3f972e2733fd337e53c7f136c0efe16b0713b34
baimao <baimao@gmail.com>
2025-09-14 17:15:24 +0800

    live-edit

--- components/liveVideo.vue
+++ components/liveVideo.vue
@@ -19,56 +19,55 @@
         </div>
         <button @click="retryConnection" class="retry-button">手动重连</button>
       </div>
-    </div>
-
-    <section class="chatlayer" ref="section1">
-      <div class="chat">
-        <div class="goback">
-          <div class="back" @click="navigateTo('/')">{{ '主頁' }}</div>
-          <!-- 自定义声音控制按钮 -->
-          <div class="custom-sound-control">
-            <button @click="toggleMute" class="sound-button" :class="{ 'muted': isMuted }"
-              :title="isMuted ? '打开声音' : '关闭声音'">
-              <svg v-if="isMuted" class="sound-icon" viewBox="0 0 24 24" fill="currentColor">
-                <path
-                  d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z" />
-              </svg>
-              <svg v-else class="sound-icon" viewBox="0 0 24 24" fill="currentColor">
-                <path
-                  d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z" />
-              </svg>
-            </button>
+      <section class="chatlayer" ref="section1">
+        <div class="chat">
+          <div class="goback">
+            <div class="back" @click="navigateTo('/')">{{ '主頁' }}</div>
+            <!-- 自定义声音控制按钮 -->
+            <div class="custom-sound-control">
+              <button @click="toggleMute" class="sound-button" :class="{ 'muted': isMuted }"
+                :title="isMuted ? '打开声音' : '关闭声音'">
+                <svg v-if="isMuted" class="sound-icon" viewBox="0 0 24 24" fill="currentColor">
+                  <path
+                    d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z" />
+                </svg>
+                <svg v-else class="sound-icon" viewBox="0 0 24 24" fill="currentColor">
+                  <path
+                    d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z" />
+                </svg>
+              </button>
+            </div>
           </div>
-        </div>
-        <div class="chat-btm">
-          <div class="chat-lis">
-            <div class="chat-item" v-for="item in 20">
-              <div class="chat-title">
-                <div class="title-user">
-                  {{ '用戶656516' }}
+          <div class="chat-btm">
+            <div class="chat-lis">
+              <div class="chat-item" v-for="item in 20">
+                <div class="chat-title">
+                  <div class="title-user">
+                    {{ '用戶656516' }}
+                  </div>
+                  :
                 </div>
-                :
-              </div>
-              <!-- <div class="chat-item-img">
+                <!-- <div class="chat-item-img">
               <img src="https://picsum.photos/200/300" alt="">
             </div> -->
-              <div class="chat-item-text">
-                <div class="chat-text" v-if="true">
-                  {{ '哈哈哈，早知道' + item }}
-                </div>
-                <div class="chat-text-img" v-else>
-                  <img src="https://picsum.photos/200/300" alt=""></img>
+                <div class="chat-item-text">
+                  <div class="chat-text" v-if="true">
+                    {{ '哈哈哈，早知道' + item }}
+                  </div>
+                  <div class="chat-text-img" v-else>
+                    <img src="https://picsum.photos/200/300" alt=""></img>
+                  </div>
                 </div>
               </div>
-            </div>
 
-          </div>
-          <div class="chat-input">
-            <input type="text" maxlength="100" placeholder="請遵守社區秩序"></input>
+            </div>
+            <div class="chat-input">
+              <input type="text" maxlength="100" placeholder="請遵守社區秩序"></input>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   </div>
 </template>
 
